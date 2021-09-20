ae0f9935527bbcdaaaa45fcd32a9f001e3d37e5e
mridulhossain <mridulfeni94@gmail.com>
2021-09-21 01:10:28 +0600

    csharp stack

---
+++ OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // stack work lifo style(last in first out)
+
+            Stack<int> numbers = new Stack<int>();
+
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+
+            //remove from last
+            numbers.Pop();
+
+
+            //when you use peek method it get last one value from dataset
+            Console.WriteLine(numbers.Peek());
+
+
+            // contains method check value exist or not,it return boolean value
+            Console.WriteLine(numbers.Contains(1));
+
+
+            // it iteration from last.
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // you can use clear method
+            numbers.Clear();
+
+
+            // you can also use this generic version.
+        }
+    }
+}
+
+
